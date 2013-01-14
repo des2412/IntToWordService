0ee2bdb3845812f1484e6fdbdcd9fa5f6080efb6
desz <des_williams_2000@yahoo.com>
2013-01-14 19:00:14 +0000

    new Exception type thrown by service

---
+++ trunk/src/main/java/org/desz/numbertoword/exceptions/WordForNumberServiceException.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package org.desz.numbertoword.exceptions;
+
+/**
+ * @author des
+ * 
+ */
+public class WordForNumberServiceException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public WordForNumberServiceException() {
+		super();
+
+	}
+
+	public WordForNumberServiceException(String message) {
+		super(message);
+
+	}
+
+	@Override
+	public String getMessage() {
+		return super.getMessage();
+	}
+
+}
